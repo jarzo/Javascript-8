b22483d71228c86ec0b8b68d216a6339a36efea1
Michał Jarząbek <nebraska994@gmail.com>
2017-06-10 14:06:26 +0200

    continue

--- 8-pentle.js
+++ 8-pentle.js
@@ -8,10 +8,79 @@
 	 console.log(i);
  }*/
 
-var it=0;
+
+/*
+
+Pętla while
+
+*/
+/*var it=0;
 while (it < 5) {
 	console.log(it);
 	it++;
+}*/
+
+/*do while, działa tak samo jak while, ale raz się wykona,na samym początku, nawet jeśli wynik będzie fałszywy*/
+
+
+var iter=20;
+do {
+	console.log(iter);
+	iter++;
+//	console.log(iter);
+} while (iter < 10);
+
+
+/*
+
+Przerwanie Pętli -break
+
+*/
+
+/*var a = 0;
+while (a < 5) {
+	console.log(++a);
+	
+	if ( a == 5) {
+		break;
+	}
+}*/
+
+
+
+//postinkrementacja- najpierw zwiększa licznik potem wykonuje instrukcje ++i//
+
+// i=i+1 // 
+// console.log //
+
+for ( var i=1; i < 4; ){
+    console.log(++i);
+} 
+
+console.log('-----');
+for ( var i=1; i < 4;){
+    console.log(i++);
 }
 
-/*do while, działa tak samo jak while, ale raz się wykona,na samym początku*/
+
+//preinkrementacja- najpierw wykonuje instrukcje a potem zwiększa licznik i++//
+
+// console.log //
+// i= i+1 //
+
+/*
+
+Przeszukiwanie do kolejnej interakcji - continue
+
+*/
+
+for ( var b = 0; b < 6 ; ++b ) {
+	
+	if ( b ==3) {
+		continue;
+	}else {
+		console.log (b);
+	}
+	
+	console.log('--')
+}
