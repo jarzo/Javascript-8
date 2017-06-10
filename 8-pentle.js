675e0ac46ce2dd0a1f6d47d83951bfd482106d39
Michał Jarząbek <nebraska994@gmail.com>
2017-06-10 13:29:25 +0200

    while

--- 8-pentle.js
+++ 8-pentle.js
@@ -1,1 +1,17 @@
 'use strict'
+/*pętla w programowaniu pozwala nam wykonać dany kod pewną ilość razy. Pętle możemy zagnieżdżać.*/
+/* for (var i=4 ; i>=0; i--) {
+	 console.log(i);
+ }*/
+
+/*for (var i=4 ; i>=0; i--) {
+	 console.log(i);
+ }*/
+
+var it=0;
+while (it < 5) {
+	console.log(it);
+	it++;
+}
+
+/*do while, działa tak samo jak while, ale raz się wykona,na samym początku*/
